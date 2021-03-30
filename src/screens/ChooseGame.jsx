481a60afc85e4c5853bae50bf626ac30714b8c71
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2021-03-30 06:59:58 +0300

    Finished remove game

--- src/screens/ChooseGame.jsx
+++ src/screens/ChooseGame.jsx
@@ -1,17 +1,38 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export const LS = localStorage;
+LS.setItem('games', JSON.stringify([{id:3, title:'First game'}, {id:5,  title:'Awesome Game'}]))
 
 // players: [ {id, name}]
 // games: {gameId:uuid, title:''}
 
 export const ChooseGame = () => {
+  const [gamesList, setGamesList] = useState([])
   useEffect(() => {
-    const games = LS.getItem('games');
+    setGamesList(JSON.parse(LS.getItem('games')) || []);
   }, []);
-  //set
-  //get
-  return <div>Choose game</div>;
+
+  const handleRemoveGame = useCallback ( (event) => {
+    const id = parseInt(event.target.dataset.id)
+    let isDelete = window.confirm("Вы уверены что хотите удалить игру?");
+    if(!isDelete) {
+      return false
+    }
+     //Fetch games
+    let games = JSON.parse(LS.getItem('games')) //[{id,title}]
+        games = games.filter(game => game.id !== id)
+    //update games
+    localStorage.setItem('games', JSON.stringify(games))
+    setGamesList(games)
+  },[])
+  return <div>
+    <h2>Choose game</h2>
+    {gamesList.length > 0 && (
+      <ul>
+        {gamesList.map(game => <li key={game.id} >{game.title} <button data-id={game.id} onClick={handleRemoveGame}>x</button></li>)}
+      </ul>) }
+    {!gamesList.length && (<p>You don't have any game. Please, create Game</p>) }
+  </div>;
 };
 
 export default ChooseGame;
